f0431f977dc981b7bca891e01069bf135cf015c7
shankarkalmade <shankar.kalmade@gmail.com>
2016-04-26 16:38:04 +0530

    prime numbers

--- src/com/shankar/codes/PrimeNumber.java
+++ src/com/shankar/codes/PrimeNumber.java
@@ -1,5 +1,42 @@
 package com.shankar.codes;
 
 public class PrimeNumber {
+	
+	
+	public static void main(String[] args) {
+		
+		int maxLimit= 50;
+		
+		
+		printPrimeNumbers(maxLimit);
+		
+		
+		
+		
+	}
 
+	private static void printPrimeNumbers(int maxLimit) {
+		int count =0;
+		for(int i=2; i<=maxLimit; i++){
+			count =0;
+			for(int j=2; j<=i/2; j++){
+				
+				if(i%j==0){
+					count++;
+					break;
+					
+				}
+				
+			}
+			
+			if(count==0){
+				
+				System.out.println("Prime number: "+ i);
+			}
+			
+		}
+		
+		
+	}
+	
 }
